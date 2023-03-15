985a6683417a0a3102c0f5e0d187e5bd986b559b
Olivia-L-V <80695018+Olivia-L-V@users.noreply.github.com>
2023-03-15 18:08:01 +0100

    updated sqlite_flaschcards

--- model/sqllite_flashcard.py
+++ model/sqllite_flashcard.py
@@ -4,3 +4,9 @@ import sqlite3
 # Creation de la base de donnees
 def create_sqlite():
     pass
+
+# Se connecter a la base de donnees
+# flashcards_db = sqlite3.connect('sql.db')
+
+# Permettre les requetes
+# cursor = flashcards_db.cursor()
