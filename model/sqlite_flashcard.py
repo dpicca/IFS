15d247d1fbddc75dc880daf406f36a187286135b
Olivia-L-V <80695018+Olivia-L-V@users.noreply.github.com>
2023-03-22 17:46:42 +0100

    sqlite_flaschcards

--- model/sqlite_flashcard.py
+++ model/sqlite_flashcard.py
@@ -12,42 +12,57 @@ def create_sqlite():
 
 
     # Permettre les requetes...
-    # -> Apres chaque requete ?
     cursor = test.cursor()
     print('The database is open')
 
-    # Preciser et executer les requetes...
-    # Exemple :
-    query = 'select sqlite_version();'
-    cursor.execute(query)
+    # Creer table question...
+    #question_table = 'CREATE TABLE question_table (' \
+    #                 'idquestion FLOAT PRIMARY KEY NOT NULL,' \
+    #                 'question VARCHAR(40),' \
+    #                 'paquet VARCHAR(20));'
+    #cursor.execute(question_table)
 
-    # Recuperer le resultat...
-    result = cursor.fetchall()
-    print('SQLite Version is {}'.format(result))
+    # Ajouter des valeurs a la table...
+    #question_add = ("INSERT INTO question_table VALUES (1.1, 'cuisinier', 'metiers')")
+    #cursor.execute(question_add)
+    #question_add = ("INSERT INTO question_table VALUES (1.2, 'peintre','metiers' )")
+    #cursor.execute(question_add)
+    #question_add = ("INSERT INTO question_table VALUES (2.1, 'ecole','ecole')")
+    #cursor.execute(question_add)
 
-    # Fermer la permission de requetes
-    cursor.close()
+    # Afficher les valeurs de la table...
+    #cursor.execute('SELECT * FROM question_table')
+    #cartes = cursor.fetchall()
+    #for i in cartes:
+    #    print(i)
 
+    # Afficher les valeurs pour un paquet de cartes...
+    #cursor.execute('SELECT * FROM question_table WHERE paquet = "metiers"')
+    #cartes = cursor.fetchall()
+    #for i in cartes:
+    #    print(i)
 
+    # Mettre a jour les valeurs de la table...
+    #cursor.execute('UPDATE question_table SET question = "classe" WHERE paquet = "ecole";')
 
-    # Test creation table :
-    # Creer table
-    test.execute(''' CREATE TABLE testcartes
-         (FIND INT PRIMARY KEY     NOT NULL,
-         COLOR           TEXT    NOT NULL,
-         VALUE            INT);
-         ''')
-    # Ajouter valeurs a la table
-    test.execute("INSERT INTO testcartes VALUES (1, 'rouge',3 )")
-    test.execute("INSERT INTO testcartes VALUES (2, 'rouge',5 )")
-    test.execute("INSERT INTO testcartes VALUES (3, 'noir',9 )")
-    cursor2 = test.execute("SELECT * from testcartes ")
-    print("Cards : \n")
-    # Imprimer valeurs lignes
-    for row in cursor2:
-        print(row)
-    cursor2.close()
+    # Supprimer des valeurs de la table...
+    #cursor.execute('DELETE FROM question_table WHERE question = "cuisinier";')
 
+    # Supprimer une table...
+    #cursor.execute('DROP TABLE question_table;')
+
+    # Creer table reponse...
+    # answer_table = 'CREATE TABLE question_table (' \
+    #                 'idquestion FLOAT PRIMARY KEY NOT NULL,' \
+    #                 'answer VARCHAR(40),' \
+    #                 'paquet VARCHAR(20));'
+    # cursor.execute(answer_table)
+
+    # Sauvegarder le changement.
+    test.commit()
+
+    # Fermer la base de donnees
+    #test.close()
 
 
 # Appeler la fonction.
