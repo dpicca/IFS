e4b96a6e10ba89aaf5c70095b9ed4aa1de663a98
lauremargot <laure.margot@bluewin.ch>
2023-03-29 17:07:39 +0200

    Finish Login

--- view/user_login.py
+++ view/user_login.py
@@ -36,3 +36,41 @@ elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
+
+if authentication_status:
+    try:
+        if authenticator.reset_password(username, 'Reset password'):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+    try:
+        if authenticator.register_user('Register user', preauthorization=False):
+            st.success('User registered successfully')
+    except Exception as e:
+        st.error(e)
+    try:
+        username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
+        if username_forgot_pw:
+            st.success('New password sent securely')
+            # Random password to be transferred to user securely
+        else:
+            st.error('Username not found')
+    except Exception as e:
+        st.error(e)
+    try:
+        username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
+        if username_forgot_username:
+            st.success('Username sent securely')
+            # Username to be transferred to user securely
+        else:
+            st.error('Email not found')
+    except Exception as e:
+        st.error(e)
+if authentication_status:
+    try:
+        if authenticator.update_user_details(username, 'Update user details'):
+            st.success('Entries updated successfully')
+    except Exception as e:
+        st.error(e)
+with open('../config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
