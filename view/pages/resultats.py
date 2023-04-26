c0268b1030e89b14b70e17bc97c8174dbba1053c
lauremargot <laure.margot@bluewin.ch>
2023-04-26 17:43:33 +0200

    Menu

--- view/pages/resultats.py
+++ view/pages/resultats.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 from urllib.error import URLError
+from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(page_title="Résultats", page_icon="📊")
 
 st.markdown("# Mes résultats")
+
+buttonRetourMenu = st.button("Retour au menu")
+if buttonRetourMenu:
+    switch_page("MenuIFC")
