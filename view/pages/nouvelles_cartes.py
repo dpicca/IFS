8af7a9a86ea66438dbb22e000d8a7b20cbcbbd50
lauremargot <laure.margot@bluewin.ch>
2023-05-03 16:50:17 +0200

    mise en page

--- view/pages/nouvelles_cartes.py
+++ view/pages/nouvelles_cartes.py
@@ -8,6 +8,12 @@ st.set_page_config(page_title="Nouvelles cartes", page_icon="🃏")
 
 st.title("Nouvelles cartes")
 
+new_cards = st.text_input('Veuillez choisir la langue dans laquelle vous souhaiteriez traduire vos cartes :')
+
+st.markdown('Veuillez choisir le moyen de créer vos cartes :')
+manually = st.checkbox('Manuellement')
+automatically = st.checkbox('Automatiquement')
+
 buttonRetourMenu = st.button("Retour au menu")
 if buttonRetourMenu:
     switch_page("MenuIFC")
