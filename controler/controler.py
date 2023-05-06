8775a0fc0ef97445af4fff38a57374e6cd9e8562
snm03 <88044233+snm03@users.noreply.github.com>
2023-05-06 12:10:37 +0200

    fetchall final

--- controler/controler.py
+++ controler/controler.py
@@ -1,71 +1,181 @@
+#Pour la traduction
 from googletrans import Translator
 translator = Translator()
 translation = translator.translate("Der Himmel ist blau und ich mag Bananen", dest='en')
 print(translation.text)
 
+# Interaction entre frontend et backend:
 from model import sqlite_flashcard
 
 class Controller:
-    #interaction entre frontend et backend
-    def add_question(self,question,paquet)
 
+    # class QuestionTable:
+    def add_question(self, question, paquet):
         question = sqlite_flashcard.QuestionTable()
         question_add = question.add_data(question, paquet)
         return question_add.fetchall()
+
     def create_table(self):
         question = sqlite_flashcard.QuestionTable()
         question_create = question.create_table()
         return question_create.fetchall()
+
     def show_table(self):
         question = sqlite_flashcard.QuestionTable()
         question_show_t = question.show_table()
         return question_show_t.fetchall()
+
     def show_data(self, paquet):
         question = sqlite_flashcard.QuestionTable()
         question_show = question.show_data(paquet)
-        returnquestion_show.fetchall()
-    def
-    question_update = question.update_data(question, paquet)
-    res = question_update.fetchall()
-    question_delete = question.delete_data(question)
-    res = question_delete.fetchall()
-    question_delete_t = question.delete_table()
-    res = question_delete_t.fetchall()
-    question_close = question.close_sqlite()
-    res = question_delete_t.fetchall()
-    answer = sqlite_flashcard.AnswerTable()
-    answer_add = answer.add_data(answer, paquet)
-    res = answer_add.fetchall()
-    answer_create = answer.create_table()
-    res = answer_create.fetchall()
-    answer_show_t = answer.show_table()
-    answer_show = answer.show_data(paquet)
-    answer_update = answer.update_data(answer, paquet)
-    answer_delete = answer.delete_data(paquet)
-    answer_delete_t = answer.delete_table()
-    answer_close = answer.close_sqlite()
-    user = sqlite_flashcard.UserTable()
-    user_add = user.add_data(name)
-    user_create = user.create_table()
-    user_show_t = user.show_table()
-    user_show = user.show_data(i)
-    user_update = user.update_data(name, i)
-    user_delete = user.delete_data(name)
-    user_delete_t = user.delete_table()
-    user_close = user.close_sqlite()
-    answeruser = sqlite_flashcard.AnswerUserTable()
-    answeruser_add = answeruser.add_data(result)
-    answeruser_create = answeruser.create_table()
-    answeruser_show_t = answeruser.show_table()
-    answeruser_show = answeruser.show_data(i)
-    answeruser_update = answeruser.update_data(result, i)
-    answeruser_delete = answeruser.delete_data(i)
-    answeruser_delete_t = answeruser.delete_table()
-    answeruser_close = answeruser.close_sqlite()
+        return question_show.fetchall()
+
+    def update_data(self, question, paquet):
+        question = sqlite_flashcard.QuestionTable()
+        question_update = question.update_data(question, paquet)
+        return question_update.fetchall()
+
+    def delete_data(self, question):
+        question = sqlite_flashcard.QuestionTable()
+        question_delete = question.delete_data(question)
+        return question_delete.fetchall()
+
+    def delete_table(self):
+        question = sqlite_flashcard.QuestionTable()
+        question_delete_t = question.delete_table()
+        return question_delete_t.fetchall()
+
+    def close_sqlite(self):
+        question = sqlite_flashcard.QuestionTable()
+        question_close = question.close_sqlite()
+        return question_close.fetchall()
+
+    # class AnswerTable:
+    def add_answer(self, answer, paquet):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_add = answer.add_data(answer, paquet)
+        return answer_add.fetchall()
+
+    def answer_create_table(self):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_create = answer.create_table()
+        return answer_create.fetchall()
+
+    def answer_show_table(self):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_show_t = answer.show_table()
+        return answer_show_t.fetchall()
+
+    def answer_show_data(self, paquet):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_show = answer.show_data(paquet)
+        return answer_show.fetchall()
+
+    def answer_update_data(self, paquet):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_update = answer.update_data(answer, paquet)
+        return answer_update.fetchall()
+
+    def answer_delete_data(self, paquet):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_delete = answer.delete_data(paquet)
+        return answer_delete.fetchall()
+
+    def answer_delete_table(self):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_delete_t = answer.delete_table()
+        return answer_delete_t.fetchall()
+
+    def answer_close_sqlite(self):
+        answer = sqlite_flashcard.AnswerTable()
+        answer_close = answer.close_sqlite()
+        return answer_close.fetchall()
+
+    # class UserTable:
+    def add_user(self, name):
+        user = sqlite_flashcard.UserTable()
+        user_add = user.add_data(name)
+        return user_add.fetchall()
+
+    def user_create_table(self):
+        user = sqlite_flashcard.UserTable()
+        user_create = user.create_table()
+        return user_create.fetchall()
+
+    def user_show_table(self, name):
+        user = sqlite_flashcard.UserTable()
+        user_show_t = user.show_table()
+        return user_show_t.fetchall()
+
+    def user_show_data(self, i):
+        user = sqlite_flashcard.UserTable()
+        user_show = user.show_data(i)
+        return user_show.fetchall()
+
+    def user_update_data(self, name, i):
+        user = sqlite_flashcard.UserTable()
+        user_update = user.update_data(name, i)
+        return user_update.fetchall()
+
+    def user_delet_table(self):
+        user = sqlite_flashcard.UserTable()
+        user_delete = user.delete_data(name)
+        return user_delete.fetchall()
+
+    def user_delete_table(self):
+        user = sqlite_flashcard.UserTable()
+        user_delete_t = user.delete_table()
+        return user_delete_t.fetchall()
+
+    def user_close_sqlite(self):
+        user = sqlite_flashcard.UserTable()
+        user_close = user.close_sqlite()
+        return user_close.fetchall()
+
+    # class AnsweruserTable:
+    def answeruser_add_data(self, result):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_add = answeruser.add_data(result)
+        return answeruser_add.fetchalle()
+
+    def answeruser_create_table(self):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_create = answeruser.create_table()
+        return answeruser_create.fetchall()
+
+    def answeruser_show_table(self):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_show_t = answeruser.show_table()
+        return answeruser_show_t.fetchall()
+
+    def answeruser_show_data(self, i):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_show = answeruser.show_data(i)
+        return answeruser_show.fetchall()
+
+    def answeruser_update_data(self, result, i):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_update = answeruser.update_data(result, i)
+        return answeruser_update.fetchall()
+
+    def answeruser_delete_data(self, i):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_delete = answeruser.delete_data(i)
+        return answeruser_delete.fetchall()
+
+    def answeruser_delete_table(self):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_delete_t = answeruser.delete_table()
+        return answeruser_delete_t.fetchall()
+
+    def answeruser_close_sqlite(self):
+        answeruser = sqlite_flashcard.AnswerUserTable()
+        answeruser_close = answeruser.close_sqlite()
+        return answeruser_close.fetchall()
 
 if __name__ == "__main__":
-    main()
+        main()
 
-    for tuples in res:
-        for ele in tuples:
-            pass
+    #for tuples in res:
+    #    for ele in tuples:
+    #       pass
