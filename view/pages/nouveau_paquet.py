37166afce6da8e6d520d133d0ec2ce9292bd67a6
lauremargot <laure.margot@bluewin.ch>
2023-05-24 17:05:13 +0200

    nouveau_paquet update

--- view/pages/nouveau_paquet.py
+++ view/pages/nouveau_paquet.py
@@ -12,17 +12,39 @@ st.title("Nouveau paquet")
 new_paquet = st.text_input("Choisir le nom du paquet :")
 buttonCreate = st.button("Créer")
 
-if buttonCreate:
-    # New card
-    new_word = st.text_input("Nouveau mot :")
-    new_translation = st.text_input("Traduction :")
+if buttonCreate and new_paquet:
+    # Step 2: Creating new cards
+    st.write("Création de nouvelles cartes :")
+    new_words = []
+    new_translations = []
+    add_new_card = True
 
-    # Put the bottoms side by side
-    col1, col2 = st.columns(2)
-    with col1:
-        st.button("Nouvelle carte")
-    with col2:
-        st.button("Valider")
+    while add_new_card:
+        with st.form("new_card_form"):
+            new_word = st.text_input("Nouveau mot :", key="new_word")
+            new_translation = st.text_input("Traduction :", key="new_translation")
+            submitted = st.form_submit_button("Ajouter")
+
+            if submitted:
+                if new_word and new_translation:
+                    new_words.append(new_word)
+                    new_translations.append(new_translation)
+                    st.success("Nouvelle carte créée avec succès !")
+                    st.write(f"Mot : {new_word}, Traduction : {new_translation}")
+                else:
+                    st.warning("Veuillez entrer le nouveau mot et sa traduction.")
+
+        add_new_card = submitted  # Set add_new_card to False if "Ajouter" button is not clicked
+
+    # Step 4: Validation
+    if st.button("Valider"):
+        # Process the new words and translations here (e.g., save them to a database or data structure)
+        for word, translation in zip(new_words, new_translations):
+            st.write(f"Mot : {word}, Traduction : {translation}")
+        st.success("Cartes créées avec succès !")
+        # new_paquet.empty() CA NE MARCHE PAS !!!!!
+elif buttonCreate and not new_paquet:
+    st.warning("Veuillez entrer le nom du paquet.")
 
 buttonRetourMenu = st.button("Retour au menu")
 if buttonRetourMenu:
